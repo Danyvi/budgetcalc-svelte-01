6b781900055844742e26d3293c7c79cb47fa5f38
Danyvi <30378170+Danyvi@users.noreply.github.com>
2020-06-16 22:09:13 -0400

    prop drilling

---
+++ src/Expense.svelte
@@ -0,0 +1,59 @@
+<!-- <script>
+  // accessing props
+  export let expense;
+  console.log(expense);
+  
+</script>
+
+<div>
+  <h2>Title: {expense.name}</h2>
+  <h2>Amount: {expense.amount}</h2>
+</div> -->
+
+<!-- <script>
+  // accessing props
+  export let expense;
+  let {name, amount, id} = expense; // destructuring
+  
+</script>
+
+<div>
+  <h2>Title: {name}</h2>
+  <h2>Amount: {amount}</h2>
+</div> -->
+
+
+<script>
+  // accessing props
+  export let id;
+  export let name = "";
+  export let amount = 0;
+  export let removeExpense;
+
+  let displayAmount = false;
+  function toggleAmount() {
+    displayAmount = !displayAmount;
+  }
+</script>
+
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name} 
+      <button class="amount-btn" on:click={toggleAmount}>
+        <i class="fas fa-caret-down"></i>
+      </button>
+    </h2>
+    {#if displayAmount}
+      <h4>Amount: ${amount}</h4>
+    {/if}
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn">
+      <i class="fas fa-pen"></i>
+    </button>
+    <button class="expense-btn delete-btn" on:click="{ () => removeExpense(id) }">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>
+</article>
